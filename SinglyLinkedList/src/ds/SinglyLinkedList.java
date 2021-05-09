20edd3702ddda6e81558a39a6fdbec48da8c8751
Hamza Nazli <chemhamza@gmail.com>
2021-05-09 23:19:24 +0530

    SinglyLinkedList Class Structure created

---
+++ SinglyLinkedList/src/ds/SinglyLinkedList.java
@@ -0,0 +1,31 @@
+package ds;
+
+public class SinglyLinkedList {
+    public void add(int number){
+
+    }
+    public void add(int index,int number){
+
+    }
+    public void remove(int index){
+
+    }
+    public int get(int index){
+        return 0;
+    }
+    public void print(){
+
+    }
+    public void clear(){
+
+    }
+    public int size(){
+        return 0;
+    }
+    public boolean contains(int number){
+        return false;
+    }
+    public boolean empty(){
+        return false;
+    }
+}
